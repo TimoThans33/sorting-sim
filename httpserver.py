6f32d6076e8aa91b0e18d533794ab99f62099cf3
Timo Thans <57143751+TimoThans33@users.noreply.github.com>
2021-01-12 20:54:18 +0100

    Delete httpserver.py

--- httpserver.py
+++
@@ -1,8 +0,0 @@
-from BaseHTTP import BaseHTTPRequestHandler, HTTPServer
-import os
-# Create custom HTTPRequestHandler class
-class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
-
-    # handle GET command
-    def do_GET(self):
-        rootdir = 'c:/xamp'
