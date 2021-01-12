b8c96085876e9d1e1b064fc08e3629e5c93b9a7f
Timo Thans <57143751+TimoThans33@users.noreply.github.com>
2021-01-12 20:54:29 +0100

    Delete initiate.py

--- initiate.py
+++
@@ -1,9 +0,0 @@
-from sanic import response
-from sanic import Blueprint
-
-my_bp = Blueprint('set_direction')
-
-@my_bp.route('/set_direction')
-
-def my_bp_func(request):
-    return response.text('set directions')
